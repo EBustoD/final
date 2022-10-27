f1e3db2085de32fb8b7cc8831bad4c75a2d818b1
EBustoD <103273058+EBustoD@users.noreply.github.com>
2022-10-27 12:03:07 +0200

    Update visualizar_datos_almacenados.kt
    Cambiar orden de listview

--- app/src/main/java/org/tensorflow/codelabs/objectdetection/visualizar_datos_almacenados.kt
+++ app/src/main/java/org/tensorflow/codelabs/objectdetection/visualizar_datos_almacenados.kt
@@ -47,6 +47,7 @@ class visualizar_datos_almacenados: AppCompatActivity() {
 
         // access the listView from xml file
         //var mListView = findViewById<ListView>(R.id.lecturasList)
+        lecturas.reverse()
         arrayAdapter = ArrayAdapter(this,
             android.R.layout.simple_list_item_1, lecturas)
         listViewLecturas.adapter = arrayAdapter
